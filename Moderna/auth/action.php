b2669e0e3989058d01acaa59d60b8646d1935c3a
Richard Tandy Japutra <richardtandy2015@gmail.com>
2022-05-21 21:14:34 +0700

    DB

--- Moderna/auth/action.php
+++ Moderna/auth/action.php
@@ -1,10 +1,35 @@
 <?php
 
+require_once '../DB/conn.php';
+
 $username = $_POST['username'];
 $pass = $_POST['password'];
 
-if($username == "worker"){
+
+$sql = "SELECT * FROM user JOIN role ON role.id_role = user.id_role 
+        WHERE username='$username' AND password='$pass'";
+$query = mysqli_query($conn,$sql);
+
+$data = mysqli_fetch_assoc($query);
+
+
+switch ($data['role_id']) {
+    case 1:
+        # code...
+        break;
+    case 2:
+        # code...
+    break;
+    case 3:
+        
+    break;
+    default:
+        # code...
+    break;
+}
+
+if($data){
     header("Location: ../employee/index.html");
 }else{
-    header("Location: ../manager/index.html");
+    echo 'gk masuk';
 }
