aa555e1bdc1abfd987c83ac41ac289d3b39724dd
ikow-g <muh.djatmieka@gmail.com>
2022-05-22 19:46:23 +0700

    role

---
+++ Moderna/admin/role/index.php
@@ -0,0 +1,52 @@
+
+<?php include '../template/header.php' ?>
+<?php include '../template/sidebar.php' ?>
+
+<body class="g-sidenav-show   bg-gray-100">
+  <main class="main-content position-relative border-radius-lg ">
+    <div class="container-fluid py-4">
+      <div class="row">
+        <div class="col-12">
+          <div class="card mb-4">
+            <div class="card-header pb-0">
+              <h6>Task</h6>
+            </div>
+            <div class="card-body px-0 pt-0 pb-2">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center mb-0">
+                  <thead>
+                    <tr>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Role</th>
+                      <!-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Submitted before</th> -->
+                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
+                      <th class="text-secondary opacity-7"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="align-middle text-center">
+                          <!-- <?= $i++ ?> -->
+                      </td>
+                      <td>
+                        <!-- <p class="text-xs font-weight-bold mb-0">Role</p> -->
+                      </td>
+                      <td class="align-middle text-center">
+                        <!-- <a href="add.html">
+                        <i class="fa fa-upload" aria-hidden="true"></i>
+                        </a> -->
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>            
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</body>
+
+<?php include '../template/footer.php' ?>
