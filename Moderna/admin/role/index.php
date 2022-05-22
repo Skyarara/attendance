81dc544f938fa105c55bbaa6fd6ce52e13b54d19
ikow-g <muh.djatmieka@gmail.com>
2022-05-22 19:51:55 +0700

    role

--- Moderna/admin/role/index.php
+++ Moderna/admin/role/index.php
@@ -9,7 +9,7 @@
         <div class="col-12">
           <div class="card mb-4">
             <div class="card-header pb-0">
-              <h6>Task</h6>
+              <h6>Role</h6>
             </div>
             <div class="card-body px-0 pt-0 pb-2">
               <div class="table-responsive p-0">
@@ -17,9 +17,7 @@
                   <thead>
                     <tr>
                       <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">No</th>
-                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Role</th>
-                      <!-- <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
-                      <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Submitted before</th> -->
+                      <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Role</th><th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Submitted before</th> -->
                       <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Action</th>
                       <th class="text-secondary opacity-7"></th>
                     </tr>
